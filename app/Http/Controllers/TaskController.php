56e28e1e8392cc4e8b07af784b96efafcb4793f2
Marco López <mlopezdev@gmail.com>
2018-02-24 03:56:38 -0500

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = Task::orderBy('id', 'DESC')->paginate(2);
+        return [
+            'pagination' => [
+                'total'         => $tasks->total(),
+                'current_page'  => $tasks->currentPage(),
+                'per_page'      => $tasks->perPage(),
+                'last_page'     => $tasks->lastPage(),
+                'from'          => $tasks->firstItem(),
+                'to'            => $tasks->lastItem(),
+            ],
+            'tasks' => $tasks
+        ];
+    }
+
+    public function show($id) {
+        $task = Task::findOrFail($id);
+        return $task;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //no se usa porque se llama al formulario desde un modal con bootstrap
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'keep' => 'required'
+        ]);
+
+        Task::create($request->all());
+        return;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //no se usa porque se llama al formulario desde un modal con bootstrap
+        /* $task = Task::findOrFail($id);
+        //Formulario
+        return $task; */
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'keep' => 'required',
+        ]);
+
+        Task::find($id)->update($request->all());
+        return;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+    }
+}
